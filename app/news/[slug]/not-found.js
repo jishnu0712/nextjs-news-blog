75cd6c3500a7bdf08df3b8d940f0f0c6bb2e6cea
jishnu <jishnu.dev.roy@gmail.com>
2024-06-18 12:48:34 +0530

    app router links setup

---
+++ app/news/[slug]/not-found.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function NotFoundPage() {
+  return (
+    <div className='error'>
+        <h1>Not found!</h1>
+        <p>The requested resource or page not found.</p>
+    </div>
+  )
+}
